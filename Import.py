8314582175d12edd633c63ec06be95e9533497fe
MackRoe <elaine.music@students.makeschool.com>
2019-10-06 16:14:44 -0700

    Added MongoDB

---
+++ Import.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import pymongo
+import json
+import os
+
+""" python template to import a CSV file into mongodb:
+    code credit to Loginworks Softwares Inc, 4870 Sadler Road,
+    Suite 300-Office 319 Glen Allen, VA 23060"""
+
+ 
+def import_csvfile(/data/AG_Complete_Files.csv):
+    mng_client = pymongo.MongoClient('localhost', 27017)
+    mng_db = mng_client['contractor/db'] # Replace mongo db name
+    collection_name = 'product_catalog' # Replace mongo db collection name
+    db_cm = mng_db[collection_name]
+    cdir = os.path.dirname(__file__)
+    file_res = os.path.join(cdir, filepath)
+    data = pd.read_csv(file_res)
+    data_json = json.loads(data.to_json(orient='records'))
+    db_cm.remove()
+    db_cm.insert(data_json)
+
+
+
+if __name__ == "__main__":
+ filepath = 'AG_Complete_Files.csv' # pass csv file path
+ import_csvfile(filepath)
--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
 from flask import render_template
+from pymongo import MongoClient
+client = MongoClient()
+db = client.Contractor
+product_catalog = db.product_catalog
+# product_catalog = read(AG_Contractor_Files.csv)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    message = "Hello World"
-    return render_template('index.html', message=message)
+def products_index():
+    # message = "Hello World"
+    # --- commented out experimental code ---
+    # f = open('AG_Complete_Files.csv', 'r')
+    # product_catalog_items = f.readlines()
+    # f.close()
+    # product_catalog = product_catalog[0].split(',')
+    # product_catalog = list(product_catalog)
+    # --- end commented out code ---
+    title = "Buy This Stuff"
+    return render_template('index.html', title=title)
+    # --- removed from return ---
+    # product_catalog=product_catalog.find()
+    # --- ----- ----- ----- ----- ---
+
 
 
 if __name__ == "__main__":
