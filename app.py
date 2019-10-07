213378afbc163fd5df1c18e7ff2b116549b9325d
MackRoe <elaine.music@students.makeschool.com>
2019-10-06 17:26:06 -0700

    begin developing flask render

--- app.py
+++ app.py
@@ -11,20 +11,28 @@ app = Flask(__name__)
 @app.route("/")
 def products_index():
     # message = "Hello World"
-    # --- commented out experimental code ---
-    # f = open('AG_Complete_Files.csv', 'r')
-    # product_catalog_items = f.readlines()
-    # f.close()
-    # product_catalog = product_catalog[0].split(',')
-    # product_catalog = list(product_catalog)
-    # --- end commented out code ---
+    
+    # load_catalog() # -- comment out to debug --
     title = "Buy This Stuff"
     return render_template('index.html', title=title)
     # --- removed from return ---
     # product_catalog=product_catalog.find()
     # --- ----- ----- ----- ----- ---
 
+def load_catalog():
+    f = open('AG_Complete_Files.csv', 'r')
+    products = f.readlines()
+    f.close()
+    products = products[0].split(',')
+    products = list(products)
+
+
+def catalog_sort():
+    # sort items from AG_Complete_Files.csv
+    # as either product codes or product descriptions
+    pass
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    load_catalog()
