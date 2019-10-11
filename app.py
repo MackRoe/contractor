e2056e03118638ea8957a9adfef7d6c86c761b73
MackRoe <elaine.music@students.makeschool.com>
2019-10-10 20:31:13 -0700

    test heroku

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
-# from cart import Cart
+
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/contractor')
 client = MongoClient(host=f'{host}?retryWrites=false')
