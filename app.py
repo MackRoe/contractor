90a6c6f19fa40f24d812ef128324545a95c3423a
MackRoe <elaine.music@students.makeschool.com>
2019-10-10 19:34:38 -0700

    Can now CRUD products

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
+# from cart import Cart
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/contractor')
-client = MongoClient()
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.Contractor
-products= db.products
+products = db.products
 cart = db.cart
 members = db.members
 url_base = "https://www.azuregreen.net/images/"
 
 
-client = MongoClient(host=f'{host}?retryWrites=false')
+
 
 db = client.get_default_database()
 
@@ -56,11 +57,93 @@ def signup_submit():
     return redirect(url_for('main'))
 
 
-@app.route('/product.html')
-def show_product():
+@app.route('/<product_id>')
+def show_product(product_id):
     """ show individual product detail """
-    return render_template(product.html)
+    product = products.find_one({'_id': ObjectId(product_id)})
+    return render_template('product.html', product=product)
 
+
+@app.route('/new_product')
+def product_new():
+    """Allow user to create a product"""
+    return render_template('product_create.html')
+
+
+@app.route('/product_create', methods=["POST"])
+def product_create():
+    """Create a product"""
+    product = {
+        'name': request.form.get('name'),
+        'desc': request.form.get('desc'),
+        'price': request.form.get('price')
+    }
+    product_id = products.insert_one(product).inserted_id
+    return redirect(url_for('show_product', product_id=product_id))
+
+
+@app.route('/<product_id>/delete', methods=["POST"])
+def product_delete(product_id):
+    """Delete a product"""
+    products.delete_one({"_id": ObjectId(product_id)})
+    return redirect(url_for('product_index'))
+
+
+@app.route('/product_list')
+def product_index():
+    """Show all products."""
+    return render_template('product_list.html', products=products.find())
+
+
+@app.route('/<product_id>/edit')
+def product_edit(product_id):
+    """Show the form to edit a product"""
+    product = products.find_one({"_id": ObjectId(product_id)})
+    return render_template('product_edit.html', product=product)
+
+
+@app.route('/<product_id>/edit', methods=["POST"])
+def product_update(product_id):
+    """Allow a user to update a product"""
+    updated_product = {
+        'name': request.form.get('name'),
+        'desc': request.form.get('desc'),
+        'price': request.form.get('price')
+    }
+    products.update_one(
+        {"_id": ObjectId(product_id)},
+        {"$set": updated_product})
+    return redirect(url_for('show_product', product_id=product_id))
+
+
+@app.route("/cart")
+def cart():
+    """ renders cart page - R of CRUD """
+    return render_template('cart.html', cart=cart.find())
+
+@app.route("/updated_cart")
+def create_cart_item():
+    """ if prod_code submit then add to cart """
+    if buy == True:
+        updated_cart = {
+            'name': request.db.get('name'),
+            'price': request.db.get('price')
+        }
+    cart.update_one(
+        {'_id': ObjectId(product_id)},
+        {'$set': updated_cart})
+
+
+
+def update_cart_item():
+
+    pass
+
+def remove_cart_item():
+    pass
+
+def calc_cart_total():
+    pass
 
 # ---- not this time Tilda ----
 # def load_catalog():
